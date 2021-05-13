6189805e43485bb1b859d096cbbdbc687a99c5e1
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2021-05-13 09:59:32 +0900

    rails 6.1

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,41 +1,38 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121081242) do
+ActiveRecord::Schema.define(version: 2017_01_21_081242) do
 
   create_table "post_automatic_translations", force: :cascade do |t|
-    t.integer  "post_id",                             null: false
-    t.string   "locale",                              null: false
-    t.boolean  "title_automatically", default: false, null: false
-    t.boolean  "text_automatically",  default: false, null: false
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.integer "post_id", null: false
+    t.string "locale", null: false
+    t.boolean "title_automatically", default: false, null: false
+    t.boolean "text_automatically", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_7092726451dc2e65910ef70f56e3893a"
   end
-
-  add_index "post_automatic_translations", ["post_id"], name: "index_7092726451dc2e65910ef70f56e3893a"
 
   create_table "post_translations", force: :cascade do |t|
-    t.integer  "post_id",    null: false
-    t.string   "locale",     null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "title"
-    t.text     "text"
-    t.string   "author"
+    t.integer "post_id", null: false
+    t.string "locale", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.text "text"
+    t.string "author"
+    t.index ["locale"], name: "index_post_translations_on_locale"
+    t.index ["post_id"], name: "index_post_translations_on_post_id"
   end
-
-  add_index "post_translations", ["locale"], name: "index_post_translations_on_locale"
-  add_index "post_translations", ["post_id"], name: "index_post_translations_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
